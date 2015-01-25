25fe7f4a31e223d73040001ce6d35d0fb46cc95b
pkillean <pkillean@gmail.com>
2015-01-25 08:04:55 -0500

    intial setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-#...
+###Global Seed
+https://github.com/Famous/global-seed
+
+###Global URL
+http://code.famo.us/famous/0.3/famous-global.js
+
+###Min URL
+http://code.famo.us/famous/0.3/famous-global.min.js
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.famous/famous "0.3"
+  :description "Famo.us"
+  :url "https://github.com/Famous/famous"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
