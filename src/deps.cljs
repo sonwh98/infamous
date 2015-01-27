71b6f47b462086fe78cc051c21e3de0d85dfbe6b
pkillean <pkillean@gmail.com>
2015-01-27 07:44:23 -0500

    swapped dashes for underscores

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,6 @@
 {:foreign-libs
- [{:file "famous/famous-global.js"
+ [{:file "famous/famous_global.js"
    :file-min "famous/famous.min.js"
    :provides ["com.famous.famous"]}]
  ;TODO, see http://swannodette.github.io/2014/03/14/externs-got-you-down/
- :externs ["famous/famous-global.js"]}
+ :externs ["famous/famous_global.js"]}
