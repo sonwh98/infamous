a8b598a87f4079968a336d15af06ebf320671d4b
pkillean <pkillean@gmail.com>
2015-01-27 17:11:54 -0500

    copy global instead of using same file for extern

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-###Global Seed
 https://github.com/Famous/global-seed
 
-###Global URL
+
 http://code.famo.us/famous/0.3/famous-global.js
 
-###Min URL
+
 http://code.famo.us/famous/0.3/famous-global.min.js
--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,8 @@
-{:foreign-libs
- [{:file "famous/famous_global.js"
-   :file-min "famous/famous.min.js"
-   :provides ["com.famous.famous"]}]
- ;TODO, see http://swannodette.github.io/2014/03/14/externs-got-you-down/
- :externs ["famous/famous_global.js"]}
+{
+   :foreign-libs
+   [{:file "famous/famous_global.js"
+     :file-min "famous/famous_global.min.js"
+     :provides ["com.famous.Famous"]}]
+   ;; TODO, see http://swannodette.github.io/2014/03/14/externs-got-you-down
+   :externs ["famous/externs.js"]
+ }
