d343854fba7baf3a6b3f93ed8b9d6aee5a769155
sto <Son.C.To@gmail.com>
2015-06-12 22:25:33 -0400

    revert to previous revision

--- examples/lesson-carousel-starter-kit/src/carousel/core.cljs
+++ examples/lesson-carousel-starter-kit/src/carousel/core.cljs
@@ -40,34 +40,27 @@
 (defn Dots [node options]
   (let [numPages (:numPages options)
         dotWidth (or (:dotWidth options) 10)
-        spacing (or (:spacing options) 5)
         dots (for [i (range numPages)
                    :let [dotNode (.. node addChild)]]
                (do
-                 (.. dotNode
-                     (setSizeMode 1 1)
-                     (setAbsoluteSize dotWidth dotWidth))
-                 (Dot dotNode)))
-        layoutDots (fn [size]
-                     (let [totalDotSize (+ (* dotWidth numPages)
-                                           (* spacing (- numPages 1)))
-                           start (->  (- (first size) totalDotSize) (/ 2))
-                           ]
-                       (doseq [i (range numbPages)
-                               :let [x (+ start (* i (+ dotWidth spacing)))
-                                     dot (nth dots i)]]
-                         (..  (:node dot) (setPosition x 0 0 )))))
-        dots-obj {:node node
-                  :dots dots
-                  :dotWidth dotWidth
-                  :spacing  spacing
-                  :numPages numPages}
+                 (.. dotNode (setSizeMode 1 1))
+                 (.. dotNode (setAbsoluteSize dotWidth dotWidth))
+                 (Dot dotNode)
+                 ))
         resizeComponent {:onSizeChange (fn [size]
-                                         (layoutDots size))}
-        ]
+                                         )}]
     (.. (first dots) select)
     (.. node (addComponent resizeComponent))
-    dots-obj))
+    
+    {:node node
+     :dots dots
+     :dotWidth dotWidth
+     :spacing  (or (:spacing options) 5)
+     :numPages numPages
+     :layoutDots (fn [size]
+                   )
+     })
+  )
 
 
 (defn Carousel [selector data]
