26fa759e2e751e8dc5b71c5111b3e6f89b56fbff
sto <Son.C.To@gmail.com>
2015-06-12 20:29:21 -0400

    step1-HelloFamous

--- examples/lesson-carousel-starter-kit/src/carousel/core.cljs
+++ examples/lesson-carousel-starter-kit/src/carousel/core.cljs
@@ -8,80 +8,17 @@
 (defonce FamousEngine (.. famous -core -FamousEngine))
 (defonce GestureHandler (.. famous -components -GestureHandler))
 
-(defn Arrow [node options]
-  (let [el (DOMElement. node)
-        gestures (GestureHandler. node)
-        direction (.. options -direction)
-        arrow-direction (if (= direction 1)
-                          ">"
-                          "<")]
-    (.. el (setProperty "color" "white"))
-    (.. el (setContent arrow-direction))
-    (.. el (setProperty "fontSize" "40px"))
-    (.. el (setProperty "lineHeight" "40px"))
-    (.. el (setProperty "cursor" "pointer"))
-    (.. el (setProperty "textHighlight" "none"))
-    (.. el (setProperty "zIndex" "2"))
-
-    (.. gestures (on "tap" (fn []
-                             (.. node (emit "pageChange" (clj->js {:direction direction})))
-                             )))
-    {:node      node
-     :el        el
-     :direction direction
-     :gestures  gestures})
-
-  )
-
-(defn Dot [node]
-  (let [el (DOMElement. node)]
-    (.. el (setProperty "borderRadius" "5px"))
-    (.. el (setProperty "border" "2px solid white"))
-    (.. el (setProperty "boxSizing" "border-box"))
-
-    {:node node
-     :el el
-     :select (fn []
-               (.. el (setProperty "backgroundColor" "white")))
-     :deselect (fn []
-                 (.. el (setProperty "backgroundColor" "transparent")))}))
-
-(defn Dots [node options]
-  (let [numPages (:numPages options)
-        dotWidth (or (:dotWidth options) 10)
-        dots (for [i (range numPages)
-                   :let [dotNode (.. node addChild)]]
-               (do
-                 (.. dotNode (setSizeMode 1 1))
-                 (.. dotNode (setAbsoluteSize dotWidth dotWidth))
-                 (Dot dotNode)
-                 ))
-        resizeComponent {:onSizeChange (fn [size]
-                                         )}]
-    (.. (first dots) select)
-    (.. node (addComponent resizeComponent))
-    
-    {:node node
-     :dots dots
-     :dotWidth dotWidth
-     :spacing  (or (:spacing options) 5)
-     :numPages numPages
-     :layoutDots (fn [size]
-                   )
-     })
-  )
-
 (defn Carousel [selector data]
   (let [context (.. FamousEngine (createScene selector))
         root (.. context addChild)
+        el (DOMElement. root)
+        _ (.. el (setContent "Hello World")
+              (setProperty "font-size" "40px")
+              (setProperty "color" "white"))
         carousel-obj {:context  context
                       :root     root
-                      :pageData data
-                      :arrows {:back (Arrow (.. root addChild) {:direction -1})
-                               :next (Arrow (.. root addChild) {:direction 1})}}
-        ]
-    
-    carousel-obj
-    ))
+                      :el el}]    
+    carousel-obj))
 
+(Carousel "body" {})
 (.. FamousEngine init)
