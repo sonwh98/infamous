c52b48891d8c8318dd1aff69fd2fac3d291f1ad6
sto <Son.C.To@gmail.com>
2015-06-14 04:19:36 -0400

    refactor: rm Arrow

--- examples/lesson-carousel-starter-kit/src/carousel/core.cljs
+++ examples/lesson-carousel-starter-kit/src/carousel/core.cljs
@@ -8,24 +8,6 @@
 (defonce FamousEngine (.. famous -core -FamousEngine))
 (defonce GestureHandler (.. famous -components -GestureHandler))
 
-(defn Arrow [node options]
-  (let [direction (:direction options)
-        arrow-direction (if (= direction 1)
-                          ">"
-                          "<")
-        el (.. (DOMElement. node)
-               (setProperty "color" "white")
-               (setContent arrow-direction)
-               (setProperty "fontSize" "40px")
-               (setProperty "lineHeight" "40px")
-               (setProperty "cursor" "pointer")
-               (setProperty "textHighlight" "none")
-               (setProperty "zIndex" "2"))]
-    {:node      node
-     :el        el
-     :direction direction}))
-
-
 (defn decorate-arrow-node [arrow-node text]
   (.. (DOMElement. arrow-node)
                (setProperty "color" "white")
