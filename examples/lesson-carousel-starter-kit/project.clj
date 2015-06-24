046a196059879f58d0e92c86be4794cad5f65ed6
sto <Son.C.To@gmail.com>
2015-06-24 01:28:04 -0400

    upgrade famous 0.6.2

--- examples/lesson-carousel-starter-kit/project.clj
+++ examples/lesson-carousel-starter-kit/project.clj
@@ -8,7 +8,7 @@
                            [org.clojure/clojurescript "0.0-3308"]
                            [figwheel "0.3.3"]
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                           [com.famous/famous "0.6.0"]
+                           [com.famous/famous "0.6.2"]
                            [reagent "0.5.0"]]
 
             :plugins [[lein-cljsbuild "1.0.6"]
