c898c280d082bd5d108e8e08e39dbb672e07b6bd
sto <Son.C.To@gmail.com>
2015-06-24 01:26:01 -0400

    upgrade famous 0.6.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.famous/famous "0.6.0"
+(defproject com.famous/famous "0.6.2"
   :description "Famo.us"
   :url "https://github.com/idiotslowdown/famous-cljs"
   :license {:name "Eclipse Public License"
