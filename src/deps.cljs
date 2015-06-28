ca5d22414ce7f121b7118b1a160196fe7d15cc6e
sto <Son.C.To@gmail.com>
2015-06-28 07:50:14 -0400

    famous_global.js -> famous.js famous_global.min.js -> famous.min.js

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,7 +1,7 @@
 {
    :foreign-libs
-   [{:file "famous/famous_global.js"
-     :file-min "famous/famous_global.min.js"
+   [{:file "famous/famous.js"
+     :file-min "famous/famous.min.js"
      :provides ["com.famous.Famous"]}]
    ;; TODO, see http://swannodette.github.io/2014/03/14/externs-got-you-down
  }
