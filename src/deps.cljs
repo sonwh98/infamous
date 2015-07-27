5467c9b9340acc28a193321f82abb28033b16b0d
sto <Son.C.To@gmail.com>
2015-07-27 14:19:23 +0100

    core.cljs

--- src/deps.cljs
+++ src/deps.cljs
@@ -2,6 +2,6 @@
    :foreign-libs
    [{:file "famous/famous.js"
      :file-min "famous/famous.min.js"
-     :provides ["com.famous.Famous"]}]
+     :provides ["com.kaicode.Famous"]}]
    ;; TODO, see http://swannodette.github.io/2014/03/14/externs-got-you-down
  }
